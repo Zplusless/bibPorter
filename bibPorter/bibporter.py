6ffdd64349493afb7927998b1f1acff1ed7305c0
Zplusless <liyuanzhe351@163.com>
2020-01-14 17:51:17 +0800

    增加命令行参数

--- bibPorter/bibporter.py
+++ bibPorter/bibporter.py
@@ -1,24 +1,32 @@
 import bibtexparser as bp
 import os
-from util import *
+import argparse
+from util import modify_bib
 
-originfile = '../bib/test.bib'
-bibfile = originfile.replace('.bib','_modified.bib')
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', help='the path of origin bib file expoted by zotero', required=True)
+parser.add_argument('-o', '--output', help='the path of bib file you are using for latex. If not given, bibPorter will use the file name in .tex file')
+args = parser.parse_args()
+
+originfile = modify_bib(args.input)
+# originfile = modify_bib(r'F:\博士学习\1.论文写作\zotero导出完整数据库.bib')
+bibfile = args.output if args.output else r'G:\0.SyncThing_全局备份\同步代码项目\bibPorter\bib\test.bib'
 
 
 cwd = os.getcwd()
 print('当前路径：'+cwd)
 
-with open(bibfile, encoding='utf8') as b_file:
+with open(originfile, encoding='utf8') as b_file:
     bibdata = bp.load(b_file)
 
-print(bibdata.entries)
+# print(bibdata.entries)
 
+# 对bib库进行格式处理
 for d in bibdata.entries:
     if 'file' in d.keys():
         del d['file']
     if 'keywords' in d.keys():
         del d['keywords']
 
-with open(bibfile.replace('modified', 'pro'), 'w', encoding='utf8') as bib_write:
+with open(bibfile, 'w', encoding='utf8') as bib_write:
     bp.dump(bibdata, bib_write)
--- bibPorter/util.py
+++ bibPorter/util.py
@@ -10,16 +10,30 @@ def modify_month(line:str):
         return line
 
 def modify_bib(bibfile):
+    outputfile=bibfile.replace('.bib', '_modified.bib')
+    
     with open(bibfile, encoding='utf8') as bf:
         biblines = bf.readlines()
         for i, line in enumerate(biblines):
             if 'month' in line:
                 biblines[i] = modify_month(line)
     
-    with open(bibfile.replace('.bib', '_modified.bib'), 'w', encoding='utf8') as bf_out:
+    with open(outputfile, 'w', encoding='utf8') as bf_out:
         bf_out.writelines(biblines)
 
+    return outputfile
+
 # 待增加bibkey遍历功能
+def get_bibkeys(texfile:str):
+    '''
+    遍历tex文件，返回bibkey的列表和tex文件中指向的bib文件
+    bib文件名由 \bibliography{reference} 命令确定
+    '''
+    bibkeys=[]
+    bibfile=''
+    
+    return bibkeys, bibfile
+
 
 if __name__ == "__main__":
     bibfile='../bib/test.bib'
